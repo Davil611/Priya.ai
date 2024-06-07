38a38ff04d866e3b757c76127726e6af5c215432
Davil611 <167523162+Davil611@users.noreply.github.com>
2024-06-07 22:42:07 +0530

    Create Priya.py

---
+++ Priya.py
@@ -0,0 +1,45 @@
+python
+import os
+import requests
+from telegram import Bot, Update, Message
+
+# Bot token
+token = "YOUR_BOT_TOKEN"
+
+# Get the bot
+bot = Bot(token=token)
+
+# Get the current update
+update = bot.getUpdates()[-1]
+
+# Get the message
+message = update.message
+
+# Get the user
+user = message.from_user
+
+# Get the chat
+chat = message.chat
+
+# Get the text
+text = message.text
+
+# Check if the user sent a command
+if text.startswith("/start"):
+    # Send a welcome message
+    bot.sendMessage(chat_id=chat.id, text="Welcome to the music bot! Type /help for more information.")
+elif text.startswith("/help"):
+    # Send a help message
+    bot.sendMessage(chat_id=chat.id, text="The music bot can play music from YouTube. Type /play <song name> to play a song.")
+elif text.startswith("/play"):
+    # Get the song name
+    song_name = text[6:]
+
+    # Make a request to YouTube to get the song URL
+    url = requests.get("https://www.youtube.com/results?search_query=" + song_name).json()[0]["url"]
+
+    # Open the song in VLC
+    os.system("vlc " + url)
+else:
+    # Send a message saying that the command is not recognized
+    bot.sendMessage(chat_id=chat.id, text="Sorry, I don't recognize that command. Type /help for more information.")
